d027af795fed9ec70bfdccc982e1ca3576bff637
Roxana S <60154537+Roxsara@users.noreply.github.com>
2022-01-28 15:16:17 +0200

    Add files via upload

---
+++ 1band_name_generator.py
@@ -0,0 +1,16 @@
+#100 days of code - python bootcamp udemy
+#Project 1
+
+#Generate a band name based on the city the user grew up in and the name of their pet
+
+print("Hello! Let's create a new band name!")
+
+city = input('What is the city that you grew up in? ')
+print('Awesome!')
+
+pet = input('And what is the name of your pet? ')
+print('That is a very cool name!')
+
+band = city + ' ' + pet
+
+print('Ok, so the name for your new band is: ', band)
